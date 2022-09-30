7f7df0778b0aa14b7f83491081203fb2e0a35999
sebastian.actis <sebaactis@gmail.com>
2022-09-30 15:23:33 -0300

    primer commit, leyendo datos de los input

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+
+// UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Eventos
+eventListeners();
+function eventListeners() {
+
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+
+};
+
+
+// Objeto con la info de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+};
+
+
+// Agrega datos al objeto de cita
+function datosCita(e) {
+
+    citaObj[e.target.name] = e.target.value;
+
+    console.log(citaObj);
+};
