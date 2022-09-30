c2553f372010856f9ffc4e68e72eb1e49e13f026
sebastian.actis <sebaactis@gmail.com>
2022-09-30 15:43:14 -0300

    generando y guardando nuevas citas en un objeto

--- js/app.js
+++ js/app.js
@@ -11,6 +11,58 @@ const sintomasInput = document.querySelector('#sintomas');
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
+// Clases
+
+class Citas {
+    constructor() {
+        this.citas = [];
+    }
+
+    agregarCita(cita) {
+        this.citas = [...this.citas, cita];
+
+        console.log(this.citas);
+    }
+};
+
+class UI {
+
+    imprimirAlerta(mensaje, tipo) {
+
+        // Crea el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // Agregar clase en caso de ser tipo error
+
+        if (tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-sucess');
+        }
+
+        // Mensaje de error
+
+        divMensaje.textContent = mensaje;
+
+        // Agregar al DOM
+
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        // Quitar alerta despues de 3s
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000)
+    }
+};
+
+
+
+const ui = new UI();
+const administrarCitas = new Citas();
+
+
 // Eventos
 eventListeners();
 function eventListeners() {
@@ -21,6 +73,8 @@ function eventListeners() {
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 
 };
 
@@ -41,5 +95,48 @@ function datosCita(e) {
 
     citaObj[e.target.name] = e.target.value;
 
-    console.log(citaObj);
 };
+
+// Valida y agrega una nueva cita a la clase de citas
+
+function nuevaCita(e) {
+    e.preventDefault();
+
+    // Extraer informacion del obj de cita
+
+    const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+    // Validar
+
+    if (mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '') {
+
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+        return;
+    }
+    // Generar un ID unico para cada registro / cita
+    citaObj.id = Date.now();
+
+    // Creando una nueva cita
+    administrarCitas.agregarCita({...citaObj});
+
+    // Reinicia el formulario
+    formulario.reset();
+
+    // Reinicia el objeto
+    reiniciarObj();
+
+    // Mostrar el HTML
+
+    
+
+}
+
+function reiniciarObj() {
+    citaObj.mascota = '',
+    citaObj.propietario = '',
+    citaObj.telefono = '',
+    citaObj.fecha = '',
+    citaObj.hora = '',
+    citaObj.sintomas = ''
+}
